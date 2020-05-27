abc3a24f11247edb7f5d2c3ed7a1f65f06ea8738
Anneloes <anneloes9405@hotmail.com>
2020-05-27 13:15:42 -0400

    weather data now responds to form data
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ const app = express();
 const apiKey = process.env.WEATHER_API_KEY
 const port = process.env.PORT || 3000
 
-app.get("/", function(req, res){
+app.use(bodyParser.urlencoded({extended: true}))
 
-  const cityName = "London"
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", (req, res) => {
+  // const unit = "metric"
+  const cityName = req.body.cityName
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-
+  
   https.get(url, response => {
     console.log(`statuscode is ${response.statusCode}`)
 
@@ -22,16 +28,15 @@ app.get("/", function(req, res){
       const description = weatherData.weather[0].description
       const icon = weatherData.weather[0].icon
       const iconImageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
-
+    
       res.write(`<p>The weather is currently ${description}</p>`)
       res.write(`<h1>The temperature in ${cityName} is ${temperature} &degC </h1>`)
       res.write(`<img src="${iconImageURL}" alt="weather-icon">`)
       res.send()
     })
   })
-
-
 })
+
 
 app.listen(port, function(){
   console.log(`Server is running on port ${port}`)
