5211d754a55e1be5dcc1f4a2901598eb3f4cfe58
Anneloes <anneloes9405@hotmail.com>
2020-05-26 23:33:31 -0400

    set up nodemon and env variable, now fixing issue with https get
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const app = express();
+const https = require("https")
+
+const apiKey = process.env.WEATHER_API_KEY
 
 const port = process.env.PORT ||3000
 
 app.get("/", function(req, res){
+
+  const cityName = "London"
+  const url = `api.openweathermap.org/data/2.5/weather?q={${cityName}}&appid={${apiKey}}`
+
+  https.get(url, function(response){
+    console.log(response)
+  })
+
   res.send("Hello!")
 })
 
