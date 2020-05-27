15061ad4b2019ba3e993fdeb55b1f3360f73a393
Anneloes <anneloes9405@hotmail.com>
2020-05-27 12:32:09 -0400

    back to native https method, retrieved and added weather data
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const express = require("express")
-const app = express();
+require('dotenv').config()
+const bodyParser = require("body-parser")
 const https = require("https")
 
-const apiKey = process.env.WEATHER_API_KEY
+const app = express();
 
-const port = process.env.PORT ||3000
+const apiKey = process.env.WEATHER_API_KEY
+const port = process.env.PORT || 3000
 
 app.get("/", function(req, res){
 
   const cityName = "London"
-  const url = `api.openweathermap.org/data/2.5/weather?q={${cityName}}&appid={${apiKey}}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
 
-  https.get(url, function(response){
-    console.log(response)
+  https.get(url, response => {
+    console.log(`statuscode is ${response.statusCode}`)
+
+    response.on("data", data => {
+      const weatherData = JSON.parse(data)
+      const temperature = Math.round(weatherData.main.temp - 273.15).toFixed(1)
+      const description = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const iconImageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+      res.write(`<p>The weather is currently ${description}</p>`)
+      res.write(`<h1>The temperature in ${cityName} is ${temperature} &degC </h1>`)
+      res.write(`<img src="${iconImageURL}" alt="weather-icon">`)
+      res.send()
+    })
   })
 
-  res.send("Hello!")
+
 })
 
 app.listen(port, function(){
